a74a5c2e852f03d9937acccd7b662bc8c061b248
Toma Cristian Nitu <65200697+kitzuuu@users.noreply.github.com>
2026-09-14 19:10:29 +0000

    Read Nutritionix credentials from environment variables

--- api/ApiCaller.py
+++ api/ApiCaller.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from api.ProcessResponse import process_response
 
@@ -5,8 +7,8 @@ def call_api(data):
     url = 'https://trackapi.nutritionix.com/v2/natural/nutrients'
     headers = {
         'Content-Type': 'application/json',
-        'x-app-id': '2238804f',
-        'x-app-key': '046ecb26051f4865d89afcc3b3216909'
+        'x-app-id': os.environ.get('NUTRITIONIX_APP_ID', ''),
+        'x-app-key': os.environ.get('NUTRITIONIX_APP_KEY', '')
     }
     body = {
         'query': str(data['meal'])  # Ensure the meal query is correct
